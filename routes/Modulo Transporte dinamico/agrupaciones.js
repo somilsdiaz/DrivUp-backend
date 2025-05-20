6e6431c19ff6756df0b89291ef9af56a0c707030
Julian Almario <juliandavidalmario@gmail.com>
2025-05-20 15:17:31 -0500

    Finish

--- routes/Modulo Transporte dinamico/agrupaciones.js
+++ routes/Modulo Transporte dinamico/agrupaciones.js
@@ -1,15 +1,37 @@
 import cron from 'node-cron';
 import express from 'express';
 import { agruparSolicitudesPorPMCP } from '../../workers/bk002_01_groupRequests.js';
+import { generateCombinations } from '../../workers/bk002_02_generateCombinations.js';
 
 const router = express.Router();
+
+
+async function nuevo_grupo(pool){
+  try {
+      
+     const data = await pool.query(`
+      SELECT COUNT(*) > 0 AS hay_nuevo_grupo
+FROM grupos_solicitudes_candidatos
+WHERE estado_procesamiento = 'nuevo_grupo';
+            `);         
+      return data.rows[0].hay_nuevo_grupo;
+    } catch (error) {
+      console.log("Error al obtener el dato:", error);
+    }
+  
+
+}
 
 export default (pool) => {
   // Endpoint para agrupar solicitudes
   router.post('/agrupar-solicitudes', async (req, res) => {
     try {
+    
       await agruparSolicitudesPorPMCP(pool);
-      res.status(200).json({ message: "Agrupación completada con éxito." });
+      if(await nuevo_grupo(pool)){
+        await generateCombinations(pool);
+      }
+      res.status(200).json({ message: "Agrupación y combinacion completada con éxito."});
     } catch (error) {
       res.status(500).json({ message: "Error al agrupar solicitudes." });
     }
@@ -20,6 +42,9 @@ export default (pool) => {
     try {
       console.log('Ejecutando agrupación automática cada 15 minutos...');
       await agruparSolicitudesPorPMCP(pool);
+   if(await nuevo_grupo(pool)){
+        await generateCombinations(pool);
+      }
       console.log('Agrupación automática completada.');
     } catch (error) {
       console.error('Error en la agrupación automática:', error);
